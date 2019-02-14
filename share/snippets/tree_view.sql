aa0c1d5e2e5583daa1d0a29529ae53f23c602c52
Andrey Utkin <andrey_utkin@fastmail.com>
2019-02-14 12:32:49 +0000

    snippets: tree_view: improvements

--- share/snippets/tree_view.sql
+++ share/snippets/tree_view.sql
@@ -1,19 +1,20 @@
 WITH RECURSIVE task_tree (uuid, rank) AS
 (
 	(
-		SELECT tasks.uuid, 1 as rank, tasks.description
+		SELECT tasks.uuid, 1 as rank, tasks.description, tasks.scheduled, tasks.duration
 		FROM tasks
 		WHERE uuid = 'f4bbff9c-8899-4ae2-bd51-7f044885384d'
 		ORDER BY scheduled
 	)
 	UNION ALL
 	(
-		SELECT tasks.uuid, (task_tree.rank + 1) as rank, tasks.description
+		SELECT tasks.uuid, (task_tree.rank + 1) as rank, tasks.description, tasks.scheduled, tasks.duration
 		FROM tasks, task_tree
 		WHERE tasks.parent = task_tree.uuid
 		      AND tasks.status = 'pending'
 		ORDER BY scheduled
 	)
 )
-SELECT uuid, REPEAT('*', rank) as rank, description
+SELECT REPEAT('*', rank) as rk, description, to_char(scheduled, 'MM-DD HH24:MI'::text) as scheduled, duration, uuid
 FROM task_tree
+-- JOINing at this point messes up rows ordering, avoiding it.
