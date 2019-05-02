andrey-utkin/taskdb

commit 89be911a10e46645e0fe9c4b9950759e85ea7dd5

schema: trigger to not override the provided `modified` timestamp
When pulling updates from CalDAV, `modified` is always provided in SQL
query, yet the trigger always replaced it with NOW(), causing extra
roundtrip of update propagation to CalDAV and back.

share/schema.sql
@@ -83,7 +83,9 @@ CREATE FUNCTION public.update_modified_fn() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 BEGIN
- NEW.modified := CURRENT_TIMESTAMP;
+ IF (NEW.modified IS NULL) THEN
+  NEW.modified := CURRENT_TIMESTAMP;
+ END IF;
  RETURN NEW;
 END;
 $$;

share/schema_changes/20190501-trigger-preserve-modified-if-given.sql
@@ -1,0 +1,10 @@
+REPLACE FUNCTION public.update_modified_fn() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+ IF (NEW.modified IS NULL) THEN
+  NEW.modified := CURRENT_TIMESTAMP;
+ END IF;
+ RETURN NEW;
+END;
+$$;

commit 5e9f893a8812448bd18907980a3e864556541dcd

schema: fix the condition for overriding `modified` in the trigger
It just never updated it since last change, as obviously `modified` is
never NULL in practice.

Fixes: 89be911a10e4 ("schema: trigger to not override the provided `modified` timestamp")

share/schema.sql
@@ -83,7 +83,7 @@ CREATE FUNCTION public.update_modified_fn() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 BEGIN
- IF (NEW.modified IS NULL) THEN
+ IF (NEW.modified IS NOT DISTINCT FROM OLD.modified) THEN
   NEW.modified := CURRENT_TIMESTAMP;
  END IF;
  RETURN NEW;

share/schema_changes/20190502-trigger-preserve-modified-fix.sql
@@ -1,0 +1,10 @@
+REPLACE FUNCTION public.update_modified_fn() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+ IF (NEW.modified IS NOT DISTINCT FROM OLD.modified) THEN
+  NEW.modified := CURRENT_TIMESTAMP;
+ END IF;
+ RETURN NEW;
+END;
+$$;